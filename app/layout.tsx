53e5f4a1ea7e003ff4d2d9789cd8cb09e10fb221
Bharat Poptwani <bharat.poptwani@gmail.com>
2023-10-02 22:06:31 +0530

    credentials based login authentication mechanism completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,12 @@ import { Nunito } from "next/font/google";
 // ---------- Components -------------
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+
+// ---------- Server Data -------------
+import getCurrentUser from "./actions/getCurrentUser";
 
 // ---------- Styles --------------
 import "./globals.css";
@@ -18,18 +22,21 @@ export const metadata: Metadata = {
   description: "Airbnb Clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
